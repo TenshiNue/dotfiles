e6af110d4572d71e3a3fbe765c5a07006bdbbd6a
Tenshi <tenshijojo@protonmail.com>
2024-07-27 18:23:17 -0500

    Changing install scrip

--- run_once_install.sh
+++ run_once_install.sh
@@ -1,7 +1,9 @@
+#!/bin/sh
 #Git
 sudo pacman -S --needed git base-devel
 # Basics
-sudo pacman -S firefox zsh git-delta zoxide fd bat eza unzip unrar pavucontrol neovim kdeconnect sshfs luarocks nodejs 
+sudo pacman -S firefox zsh git-delta zoxide fd bat eza unzip unrar pavucontrol neovim kdeconnect sshfs luarocks nodejs npm github-cli tree man less qbittorrent xf86-video-amdgpu mesa lib32-mesa vulkan-radeon lib32-vulkan-radeon steam discord
+
 # Rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 #yay
@@ -16,7 +18,5 @@ yay ttf-iosevka-nerd
 yay ttf-iosevkaterm-nerd
 #oh-my-posh
 curl -s https://ohmyposh.dev/install.sh | bash -s
-#Chezmoi apply
-chezmoi init --apply TenshiNue
 #Japanese fonts
 sudo pacman -S noto-fonts-cjk
