e5fe4e4e032a91b8149df3fcf8137e7f61b03113
Tenshi <tenshijojo@protonmail.com>
2024-07-27 19:55:16 -0500

    Adding clipboard manager

--- run_once_install.sh
+++ run_once_install.sh
@@ -2,7 +2,9 @@
 #Git
 sudo pacman -S --needed git base-devel
 # Basics
-sudo pacman -S firefox zsh git-delta zoxide fd bat eza unzip unrar pavucontrol neovim kdeconnect sshfs luarocks nodejs npm github-cli tree man less qbittorrent xf86-video-amdgpu mesa lib32-mesa vulkan-radeon lib32-vulkan-radeon steam discord
+sudo pacman -S firefox zsh git-delta zoxide fd bat eza unzip unrar pavucontrol neovim kdeconnect sshfs luarocks nodejs npm github-cli tree man less qbittorrent xf86-video-amdgpu mesa lib32-mesa vulkan-radeon lib32-vulkan-radeon steam discord zathura zathura-cb zathura-pdf-mupdf nsxiv cliphist
+#Yazi
+sudo pacman -S yazi ffmpegthumbnailer unarchiver jq poppler fd ripgrep fzf zoxide
 
 # Rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
@@ -20,3 +22,6 @@ yay ttf-iosevkaterm-nerd
 curl -s https://ohmyposh.dev/install.sh | bash -s
 #Japanese fonts
 sudo pacman -S noto-fonts-cjk
+#Set default shell
+chsh -s /usr/bin/zsh
+
