2d579e5c242077d6a4a2da9cbcc16f923acd43a4
Tenshi <tenshijojo@protonmail.com>
2024-07-29 09:13:57 -0500

    Wezterm font changes and starship

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,7 +1,7 @@
 wezterm = require("wezterm")
 local config = {}
 
-config.font = wezterm.font("Iosevka NF Medium")
+--config.font = wezterm.font("Iosevka NF Medium")
 config.font_size = 18
 config.color_scheme = "tokyonight_night"
 
