32a8d23856d05f3f5d6275a679af1d1471c9d9e3
Tenshi <tenshijojo@protonmail.com>
2024-07-29 21:41:01 -0500

    Adding nu shell config

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,11 +1,20 @@
 wezterm = require("wezterm")
 local config = {}
-
---config.font = wezterm.font("Iosevka NF Medium")
+config.default_prog = { "nu", "-l" }
 config.font_size = 18
 config.color_scheme = "tokyonight_night"
-
 --Remove window in gnome
 config.window_decorations = "RESIZE"
-
+--Stop close prompt
+config.window_close_confirmation = "NeverPrompt"
+--bar
+local bar = wezterm.plugin.require("https://github.com/adriankarlen/bar.wezterm")
+bar.apply_to_config(config)
+bar.apply_to_config(config, {
+	enabled_modules = {
+		username = false,
+		clock = false,
+		hostname = false,
+	},
+})
 return config
