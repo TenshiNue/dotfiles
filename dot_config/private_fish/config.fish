cf0d2e72abbdaf056a8dbf69ea1ccdd20dc7ead5
Tenshi <tenshijojo@protonmail.com>
2024-08-02 10:30:03 -0500

    Changing theme, bar and yazi for fish

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -3,4 +3,12 @@ if status is-interactive
 end
 fish_add_path ~/.local/bin/
 oh-my-posh init fish --config 'https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/catppuccin_mocha.omp.json' | source 
+function yy
+	set tmp (mktemp -t "yazi-cwd.XXXXXX")
+	yazi $argv --cwd-file="$tmp"
+	if set cwd (cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+		cd -- "$cwd"
+	end
+	rm -f -- "$tmp"
+end
 
--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -2,20 +2,24 @@ wezterm = require("wezterm")
 local config = {}
 config.default_prog = { "fish", "-l" }
 config.font_size = 18
-config.color_scheme = "Catppuccin Mocha"
+config.color_scheme = "Kanagawa Dragon (Gogh)"
 --Remove window in gnome
 config.window_decorations = "RESIZE"
 --Stop close prompt
 config.window_close_confirmation = "NeverPrompt"
 --bar
-local bar = wezterm.plugin.require("https://github.com/adriankarlen/bar.wezterm")
-bar.apply_to_config(config)
-bar.apply_to_config(config, {
-	enabled_modules = {
-		username = false,
-		clock = false,
-		hostname = false,
-		cwd = false,
-	},
-})
+config.enable_tab_bar = true
+config.hide_tab_bar_if_only_one_tab = false
+config.show_new_tab_button_in_tab_bar = true
+config.show_tab_index_in_tab_bar = false
+config.show_tabs_in_tab_bar = true
+config.switch_to_last_active_tab_when_closing_tab = false
+config.tab_and_split_indices_are_zero_based = false
+config.tab_bar_at_bottom = true
+config.tab_max_width = 25
+config.use_fancy_tab_bar = false
+
+
+
+
 return config
