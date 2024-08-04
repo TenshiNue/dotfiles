e53444ee3e1694a6991f8a7097c5ffdf8baacf37
Tenshi <tenshijojo@protonmail.com>
2024-08-03 21:30:14 -0500

    Adding lazy git to installation script

--- run_once_install.sh
+++ run_once_install.sh
@@ -11,7 +11,7 @@ git clone https://aur.archlinux.org/paru.git
 cd paru
 makepkg -si
 # Packages
-packages=("firefox" "neovim" "wl-clipboard" "git-delta" "fd" "bat" "eza" "unzip" "unrar" "pavucontrol" "sshfs" "luarocks" "nodejs" "npm" "github-cli" "tree" "man" "less" "qbittorrent" "xf86-video-amdgpu" "mesa" "lib32-mesa" "vulkan-radeon" "lib32-vulkan-radeon" "steam" "zathura" "zathura-cb" "zathura-pdf-mupdf" "nsxiv" "fish" "yazi" "ffmpegthumbnailer" "unarchiver" "jq" "poppler" "fd" "ripgrep" "fzf" "zoxide" "noto-fonts-cjk" "bibata-cursor-theme-bin" "ttf-jetbrains-mono-nerd" "ttf-ms-win11-auto" "kitty" "wezterm-git" "waydroid" "fastfetch" "ufw" "gufw" "freerdp" "remmina" "gnome-browser-connector" "mpv" "vesktop-bin" "torbrowser-launcher")
+packages=("firefox" "neovim" "wl-clipboard" "git-delta" "fd" "bat" "eza" "unzip" "unrar" "pavucontrol" "sshfs" "luarocks" "nodejs" "npm" "github-cli" "tree" "man" "less" "qbittorrent" "xf86-video-amdgpu" "mesa" "lib32-mesa" "vulkan-radeon" "lib32-vulkan-radeon" "steam" "zathura" "zathura-cb" "zathura-pdf-mupdf" "nsxiv" "fish" "yazi" "ffmpegthumbnailer" "unarchiver" "jq" "poppler" "fd" "ripgrep" "fzf" "zoxide" "noto-fonts-cjk" "bibata-cursor-theme-bin" "ttf-jetbrains-mono-nerd" "ttf-ms-win11-auto" "kitty" "wezterm-git" "waydroid" "fastfetch" "ufw" "gufw" "freerdp" "remmina" "gnome-browser-connector" "mpv" "vesktop-bin" "torbrowser-launcher" "lazygit")
 for package in "${packages[@]}"; do
   echo "$package"
   paru -S "$package"
