29e409b381b60abfbb3791c0b19f3420f9092c28
Tenshi <tenshijojo@protonmail.com>
2024-08-14 21:52:51 -0500

    Changing telescope configuration

--- dot_config/nvim/lua/plugins/telescope.lua
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -1,11 +1,31 @@
 return {
   'nvim-telescope/telescope.nvim',
-  dependencies = { 'nvim-lua/plenary.nvim' },
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'BurntSushi/ripgrep',
+    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+    'princejoogie/dir-telescope.nvim',
+  },
   config = function()
     local builtin = require 'telescope.builtin'
     vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
     vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
     vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
     vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+    vim.keymap.set('n', '<leader>fd', '<cmd>Telescope dir live_grep<CR>', { noremap = true, silent = true })
+    vim.keymap.set('n', '<leader>pd', '<cmd>Telescope dir find_files<CR>', { noremap = true, silent = true })
+    require('telescope').setup {
+      extensions = {
+        fzf = {
+          fuzzy = true, -- false will only do exact matching
+          override_generic_sorter = true, -- override the generic sorter
+          override_file_sorter = true, -- override the file sorter
+          case_mode = 'smart_case', -- or "ignore_case" or "respect_case"
+          -- the default case_mode is "smart_case"
+        },
+      },
+    }
+    require('telescope').load_extension 'fzf'
+    require('telescope').load_extension 'dir'
   end,
 }
